ca87aaec9e6934edb280617aed4e6d657542a257
rimmygit <gpfla5503@gmail.com>
2023-07-14 19:31:27 +0900

    joy 주석 추가

--- middlewares/Validations/usersValidation.js
+++ middlewares/Validations/usersValidation.js
@@ -1,10 +1,14 @@
-const Joi = require('joi');
-const { user } = require('./message.json');
+const Joi = require('joi'); // joi 모듈을 가져와서 Joi변수에 할당
+const { user } = require('./message.json'); // message.json 파일에서 User를 가져옴
 
 const userValidation = {
+  //userValidation에 signInValidation, signUpValidation,editPasswordValidation 를 담음
+
   signInValidation: async (req, res, next) => {
-    const body = req.body;
+    // 요청, 응답 및 미들웨어 체인의 다음 단계로 제어를 전달
+    const body = req.body; // 요청의 본문(body)을 body 변수에 할당
     const schema = Joi.object().keys({
+      //Joi.object().keys({ ... })를 사용하여 검증 스키마를 생성. 이 스키마는 email과 password라는 두 개의 필드를 가진 객체를 기대.
       email: Joi.string()
         .empty()
         .max(30)
